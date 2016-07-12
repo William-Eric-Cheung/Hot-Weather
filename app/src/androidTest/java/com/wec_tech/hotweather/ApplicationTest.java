67e974b284860449ebe32167601ef2fa78a77880
William Eric Cheung <William Eric Cheung>
2016-07-12 11:45:28 +0800

    First commit.

---
+++ app/src/androidTest/java/com/wec_tech/hotweather/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.wec_tech.hotweather;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
